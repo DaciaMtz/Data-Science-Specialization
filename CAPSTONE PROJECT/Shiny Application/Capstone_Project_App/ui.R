84df6573371586b31880e9e557676107fe4ea456
Dacia Martínez Díaz <daciamtz@hotmail.com>
2024-12-10 17:00:49 -0600

    SlideDeck and App deployed

--- CAPSTONE PROJECT/Shiny Application/Capstone_Project_App/ui.R
+++ CAPSTONE PROJECT/Shiny Application/Capstone_Project_App/ui.R
@@ -12,7 +12,7 @@ ui <- fluidPage(
     sidebarPanel(
       h3("Instructions"),
       p("1. Type a phrase or a sentence in the text box."),
-      p("2. Click the 'Predict Next Word' button to see suggestions. It may take a couple of seconds, please be patient"),
+      p("2. Click the 'Predict Next Word' button to see suggestions. It may take a couple of seconds, please be patient."),
       p("You can select one of the suggested words to append it to your input."),
       p("You can continue typing or selecting predictions to form a complete sentence.")
     ),
